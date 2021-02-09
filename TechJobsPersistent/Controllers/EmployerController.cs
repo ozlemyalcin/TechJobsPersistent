7b3c1ef6f168cbda92c27a23926f3b849305a1b2
ozlemyalcin <omersam33@gmail.com>
2021-02-09 17:49:53 -0600

    look adding job

--- TechJobsPersistent/Controllers/EmployerController.cs
+++ TechJobsPersistent/Controllers/EmployerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsPersistent.Data;
 using TechJobsPersistent.Models;
 using TechJobsPersistent.ViewModels;
 
@@ -12,25 +13,53 @@ namespace TechJobsPersistent.Controllers
 {
     public class EmployerController : Controller
     {
+
+        private JobDbContext context;
+        public EmployerController(JobDbContext dbcontext)
+        {
+            context = dbcontext;
+        }
+
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            List<Employer> employers = context.Employers.ToList();
+            return View(employers);
         }
 
         public IActionResult Add()
         {
-            return View();
+            AddEmployerViewModel addEmployerViewModel = new AddEmployerViewModel();
+          
+            return View(addEmployerViewModel);
         }
 
-        public IActionResult ProcessAddEmployerForm()
+        public IActionResult ProcessAddEmployerForm(AddEmployerViewModel addEmployerViewModel)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                Employer newEmployer = new Employer
+                {
+                    Name = addEmployerViewModel.Name,
+                    Location = addEmployerViewModel.Location
+                };
+                context.Employers.Add(newEmployer);
+                context.SaveChanges();
+
+                return (Redirect("/Employer/Index"));
+            }
+            return View(addEmployerViewModel);
         }
 
         public IActionResult About(int id)
         {
-            return View();
+            
+                Employer theEmployer = context.Employers.Find(id);
+
+      
+            //context.SaveChanges();
+            return View(theEmployer);
         }
     }
 }
